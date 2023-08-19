71de03315926073ed2fc860c55319e225a6a79cb
AmirBana <amir220.h@gmail.com>
2023-08-19 22:51:02 +0200

    adding camera follow

--- Assets/Game/Scripts/Core/CameraFollow.cs
+++ Assets/Game/Scripts/Core/CameraFollow.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Transform player;
+    private Vector3 distance;
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        distance = Camera.main.transform.position - player.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 pos = distance + player.position;
+        transform.position = pos;
     }
 }
